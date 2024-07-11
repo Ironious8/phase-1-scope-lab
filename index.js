e7ba28024b23531cd1c89bfbc97e2c748567f01a
Ironious8 <tyronekariuki@gmail.com>
2024-07-11 11:45:27 +0300

    Add scope

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+var customerName='bob';
+
+function upperCaseCustomerName() {
+customerName=customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    window.bestCustomer='not bob';
+    
+}
+setBestCustomer();
+
+function overwriteBestCustomer(newBestCustomer) {
+    window.bestCustomer=newBestCustomer;
+    
+}
+overwriteBestCustomer();
+
+const leastFavoriteCustomer=ted;
+const newLeastFavoriteCustomer=sam;
+function changeLeastFavoriteCustomer(leastFavoriteCustomer, newLeastFavoriteCustomer) {
+    leastFavoriteCustomer=newLeastFavoriteCustomer;
+
+}
+changeLeastFavoriteCustomer();
+
+
+
